ac215863c9e1219487174c4209712d88050930c1
junky <junky@DESKTOP-JBMGVT7>
2017-01-29 18:45:08 +0900

    as

--- src/main/java/com/sp/group/notice/NoticeController.java
+++ src/main/java/com/sp/group/notice/NoticeController.java
@@ -33,8 +33,8 @@ public class NoticeController {
 	private NoticeService service;
 	@Autowired 
 	private MyUtil myUtil;
-	@Autowired 
-	private FileManager fileManager;
+	//@Autowired 
+	//private FileManager fileManager;
 
 	@RequestMapping(value = "/gnotice")
 	public String noticeList(
