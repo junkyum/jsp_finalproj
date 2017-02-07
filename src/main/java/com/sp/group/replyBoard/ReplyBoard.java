0fb0893011084bd7f233d1e081688d7e9dd3bf41
Cheol <Cheol@DESKTOP-QKPHBQB>
2017-02-07 20:50:05 +0900

    kim

---
+++ src/main/java/com/sp/group/replyBoard/ReplyBoard.java
@@ -0,0 +1,88 @@
+package com.sp.group.replyBoard;
+
+public class ReplyBoard {
+	
+	private int listNum, responseNum, groupNumber;
+	private String userId, groupName, created;
+	private	String subject,content;
+	
+	private int depth;
+	private int orderNo;
+	private int parent;
+	private int hitcount;
+	
+	public int getListNum() {
+		return listNum;
+	}
+	public void setListNum(int listNum) {
+		this.listNum = listNum;
+	}
+	public int getResponseNum() {
+		return responseNum;
+	}
+	public void setResponseNum(int responseNum) {
+		this.responseNum = responseNum;
+	}
+	public int getGroupNumber() {
+		return groupNumber;
+	}
+	public void setGroupNumber(int groupNumber) {
+		this.groupNumber = groupNumber;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getGroupName() {
+		return groupName;
+	}
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getDepth() {
+		return depth;
+	}
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	public int getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	public int getParent() {
+		return parent;
+	}
+	public void setParent(int parent) {
+		this.parent = parent;
+	}
+	public int getHitcount() {
+		return hitcount;
+	}
+	public void setHitcount(int hitcount) {
+		this.hitcount = hitcount;
+	}
+	
+	
+}
